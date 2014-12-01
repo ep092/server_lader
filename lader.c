ef49cf08bd2017fdb48bbf34e31a2947e3844ab5
Philipp David Hörauf <philipp@Atlantis>
2014-12-01 23:04:38 +0100

    neue Bilder und Regelung für den Netzteilmodus

--- lader.c
+++ lader.c
@@ -1,32 +1,32 @@
-/*
- *    Filename: lader.c
- *     Version: 0.0.1
- * Description: Regelung für Ladung von riesigen Akkus!
- *     License: GPLv3 or later
- *     Depends: global.h, io.h, interrupt.h
- *
- *      Author: Copyright (C) Philipp Hörauf, Toni
- *        Date: 2014-11-15
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
+//  *    Filename: lader.c
+//  *     Version: 0.0.1
+//  * Description: Regelung für Ladung von riesigen Akkus!
+//  *     License: GPLv3 or later
+//  *     Depends: global.h, io.h, interrupt.h
+//  *
+//  *      Author: Copyright (C) Philipp Hörauf, Toni
+//  *        Date: 2014-11-15
+//  *
+//  *  This program is free software; you can redistribute it and/or modify
+//  *  it under the terms of the GNU General Public License as published by
+//  *  the Free Software Foundation; either version 2 of the License, or
+//  *  (at your option) any later version.
+//  *
+//  *  This program is distributed in the hope that it will be useful,
+//  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  *  GNU General Public License for more details.
+//  *
+//  *  You should have received a copy of the GNU General Public License
+//  *  along with this program; if not, write to the Free Software
+//  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ 
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdio.h>
+#include <avr/pgmspace.h>
 
 #include "../AtmelLib/global.h"
 #include "../AtmelLib/io/io.h"
@@ -66,13 +66,14 @@ uint8_t ERR=0;
 #define B_STEP_RECHTS ((knopf >>2) & 1) 
 #define RESET_STEP_RECHTS cbi(knopf, 2) 
 
-#define STROMOFFSET 593 //TODO automatische Kalibrierung
+#define MAXIMALSPANNUNG 61000 // in mV, ab hier wird der Errorstate betreten!
+
+uint16_t STROMOFFSET = 593; //TODO EINBAUEN: automatische Kalibrierung vor Ladebeginn
 
 typedef enum {
 	INIT_STATE, AUSWAHL, MODUS_LADER, MODUS_NETZGERAET, REGELUNG_NETZGERAET,
-	SPANNUNGSEINSTELLUNG, STROMEINSTELLUNG, ZAHLENWERT_AENDERN,
 	LADEN_AKTIV, LADUNG_FERTIG, ERROR_STATE
-}state_main;
+} state_main;
 
 typedef enum {
 	LADESCHLUSSSPANNUNG, MAXIMALSTROM, STROM_ENDE, START, ZURUECK
@@ -97,8 +98,6 @@ volatile uint8_t knopf=0;
 // Drehung Pin 1
 
 
-
-
 void adcInit(void) {
 	ADMUX = 0; // Referenz auf externe Referenz
 	ADCSRA = 1<<ADEN | 1<<ADSC | 1<<ADIE | 5<<ADPS0; // ADC an, Interrupt an, Prescaler=128
@@ -112,7 +111,6 @@ void adcStart(void) {
 void dacSetValue(uint16_t wert, uint8_t mode) {
 	uint8_t tempspcr = SPCR; // sichere den Stand von SPCR
 	SPCR = 1<<SPE | 1<<MSTR | 1<<CPOL; // SPI MSB first, Clock on falling edge, sample on leading, VOLLGAS
-	
 	SS_DAC(0); // SS_DAC ist low active
 	SPDR = (0b00000011 & mode); // sende PowerDown-Mode
 	while (!(SPSR & (1<<SPIF))); // warte auf Transmission complete
@@ -128,7 +126,6 @@ void dacSetValue(uint16_t wert, uint8_t mode) {
 #define RG1 3300
 #define RG2 10000
 #define DAC_REF 2.5
-#define UMAX 61000
 #define T 55.44
 #define M -4.9
 
@@ -138,13 +135,9 @@ void dacSetValue(uint16_t wert, uint8_t mode) {
 #define K1 5.0303
 #define K2 2.5
 
-void setPowerOutput(uint16_t millivolt) {
-	if (millivolt<UMAX) { // Schutz vor Überspannung
+inline void setPowerOutput(uint16_t millivolt) {
 		// Berechnung des DAC-wertes aus der sollspannung
 		dacSetValue((uint16_t)(((((((float)millivolt / 1000) - T) / M) + K2) * 65536) / (K1 * DAC_REF)), 0);
-	} else {
-		; // nix. wert wird nicht geändert
-	}
 }
 
 void timerInit(void) {
@@ -158,7 +151,6 @@ void timerInit(void) {
 // Der ADC wird interruptgesteuert
 volatile uint8_t counter = 0;
 ISR(TIMER1_COMPA_vect, ISR_BLOCK) {
-	// 	uartTxStrln("timer");
 	ADMUX = ADMUX & 0b11100000; // lösche selektiv die MUX-Bits
 	ADMUX |= (counter%3); // setze ADC-Kanal neu
 	ADCSRA |= 1<<ADSC; // start conversion
@@ -167,7 +159,7 @@ ISR(TIMER1_COMPA_vect, ISR_BLOCK) {
 
 // Drehrad Interrupts
 ISR(INT0_vect, ISR_BLOCK) {
-	if(knopf & 1<<5) { // knopf wurde eins nach links gedreht
+	if(knopf & 1<<5) { // Knopf wurde eins nach links gedreht
 		sbi(knopf, 3);
 		cbi(knopf, 5);
 	} else { // knopf wurde eins nach rechts gedreht
@@ -177,7 +169,7 @@ ISR(INT0_vect, ISR_BLOCK) {
 
 // Drehung Pin 2
 ISR(INT1_vect, ISR_BLOCK) {
-	if(knopf & 1<<4) { // knopf wurde eins nach rechts gedreht
+	if(knopf & 1<<4) { // Knopf wurde eins nach rechts gedreht
 		sbi(knopf, 2);
 		cbi(knopf, 4);
 	} else { // knopf wurde eins nach links gedreht
@@ -201,23 +193,24 @@ ISR(PCINT2_vect, ISR_BLOCK) {
 // Die Werte können noch kalibriert werden, allerdings nicht zur Laufzeit.
 
 #define REFERENZ 4	// externe Referenz, 4096mV also 4mV pro STEP
+#define ABWEICHUNG 1.000296589  // gemessene Abweichung in % vom exakten Referenzspannungswert
 #define MITTELWERTE 4
 volatile uint16_t uNetzteil=0, uReserve=0, strom=0; // U in mV, I in mA
 
 ISR(ADC_vect, ISR_BLOCK) {
 	// 	uartTxStrln("adc");
-	// ADC-Auslesungen und Rechnung mit Gleitmittelwert über 5 Werte
+	// ADC-Auslesungen und Rechnung mit Gleitmittelwert über 4 Werte
 	static uint16_t tabelle[3*MITTELWERTE]; // hier kommen die ADC-werte rein.
 	uint16_t temp=0;
 	tabelle[counter] = ADC; // fülle Tabelle mit ADC-Werten
-
+	
 	switch (counter%3) {
 		case 0:
 			// uNetzteil wird ausgerechnet
 			for(uint8_t i=0; i<MITTELWERTE; i++) {
 				temp += tabelle[0 + 3*i];
 			}
-			uNetzteil = ((temp*REFERENZ)<<2);
+			uNetzteil = (uint16_t)((temp*REFERENZ)<<2)*ABWEICHUNG;
 			break;
 			
 		case 1:
@@ -225,7 +218,7 @@ ISR(ADC_vect, ISR_BLOCK) {
 			for(uint8_t i=0; i<MITTELWERTE; i++) {
 				temp += tabelle[1 + 3*i];
 			}
-			uReserve = ((temp*REFERENZ)<<2);
+			uReserve = (uint16_t)((temp*REFERENZ)<<2)*ABWEICHUNG;
 			break;
 			
 		case 2:
@@ -234,9 +227,7 @@ ISR(ADC_vect, ISR_BLOCK) {
 				temp += tabelle[2 + 3*i];
 			}
 			temp -= STROMOFFSET;
-			//strom=temp;
-			//strom=tabelle[2]+tabelle[5]+tabelle[8]+tabelle[11];
-			strom = (uint16_t)((float)temp*((float)50/(float)3));
+			strom = (uint16_t)((float)temp*ABWEICHUNG*((float)50/(float)3));
 			break;
 	}
 	counter++;
@@ -285,47 +276,99 @@ uint8_t step_rechts(void) {
 	return return_value;
 }
 
-uint16_t spannungseinstellung(uint16_t spannung){
+uint16_t spannungseinstellung(uint16_t lokalspannung) {
 	while(knopf_losgelassen()!=1){
-		sprintf(display, "Uout: %5umV ", spannung);
+		sprintf(display, "Uout: %5umV ", lokalspannung);
 		display_set_row(2);
 		spi_write_string(display);
 		if (step_links()){
-			spannung -=100;
+			lokalspannung -=100;
 		}else if (step_rechts()){
-			spannung += 100;
-		}//TODO overflow
+			lokalspannung += 100;
+		}
 	}
-	return spannung;
+	return lokalspannung;
 }
 
-uint16_t stromeinstellung(uint16_t strom){
+uint16_t stromeinstellung(uint16_t lokalstrom) {
 	while(knopf_losgelassen()!=1){
-		sprintf(display, "Uout: %5umA ", strom);
+		sprintf(display, "Uout: %5umA ", lokalstrom);
 		display_set_row(2);
 		spi_write_string(display);
 		if (step_links()){
-			strom -=100;
-		}else if (step_rechts()){
-			strom += 100;
-		}//TODO overflow
+			lokalstrom -=100;
+		} else if (step_rechts()){
+			lokalstrom += 100;
+		}
 	}
-	return strom;
+	return lokalstrom;
 }
 
-void netzteil_regulation(uint16_t spannung, uint16_t strom){
-	//TODO
-	static uint8_t firststart=0;
+void netzteil_regulation(void) {
+	uint8_t regelspannung = 0;
+	setPowerOutput(netzgeraet_spannung);
+	delayms(200);
+	NT_ON(1);
+	display_set_row(0);
+	spi_write_pstr(PSTR("Netzgeraet AKTIV"));
+	// wenn Knopf gedrückt oder Error passiert, wieder NT-Modus verlassen
+	while ((knopf_losgelassen() == 0) && (state != ERROR_STATE)) {
+		
+		display_set_row(1);
+		sprintf(display, "Uout: %5umV ", uNetzteil); // Netzteilspannung anzeigen
+		spi_write_string(display);
+		display_set_row(2);
+		sprintf(display, "Iout: %5umA ", strom); // Laststrom anzeigen
+		spi_write_string(display);
+		
+		if ((strom > (netzgeraet_strom + 1000)) || (uNetzteil > MAXIMALSPANNUNG)) {
+			NT_ON(0); // Netzteil AUS. Überstrom- oder Überspannung!
+			state = ERROR_STATE;
+			SPKR(1);
+			delayms(1000); // TÜÜT
+			SPKR(0);
+			continue;
+			
+		} else if (uNetzteil - 100 > netzgeraet_spannung) { // Ausgangsspannung zu hoch
+			regelspannung--;
+			delayms(10); // Zeitkonstante künstlich erhöht, damit Regelung nicht schwingt.
+			
+		} else if (uNetzteil + 100 < netzgeraet_spannung) { // Ausgangsspannung zu klein
+			regelspannung++;
+			delayms(10);
+		}
+		
+		// Normaler Betrieb, Spannung kann verstellt werden
+		if (step_links() && (netzgeraet_spannung > 100)) { // Spannung um 100mV runter, minimal 0V
+			netzgeraet_spannung = netzgeraet_spannung - 100;
+		}
+		
+		if (step_rechts() && ((netzgeraet_spannung + 100) < MAXIMALSPANNUNG)) { // Spannung um 100mV hoch
+			netzgeraet_spannung = netzgeraet_spannung + 100;
+		}
+		
+		uartTxStr("U=");
+		uartTxDec(netzgeraet_spannung);
+		uartTxStrln(" mV");
+		if ((netzgeraet_spannung + regelspannung * 8) > MAXIMALSPANNUNG) {
+			; // do nothing.
+		} else {
+			setPowerOutput(netzgeraet_spannung + regelspannung * 8);
+		}
+	}
+	// ENDE. Netzteil wird abgeschaltet und State verlassen
+	NT_ON(0);
+	delayms(100);
 }
 
 
 void stateMachine(void) {
 	knopf=0;
-	while (1){
+	while (1) {
 		switch (state) {
 			case INIT_STATE:
 				uartTxStrln("INIT_STATE");
-				spi_write_string("   Guten Tag!     gebaut von    Toni und Philipp"); // Begrüßung
+				spi_write_pstr(PSTR("   Guten Tag!     gebaut von    Toni und Philipp")); // Begrüßung
 				while (knopf_losgelassen() != 1) {
 				}
 				state = AUSWAHL;
@@ -343,21 +386,21 @@ void stateMachine(void) {
 				if (knopf_losgelassen()) {
 					if (auswahl==0) {
 						state=MODUS_LADER;
-//						auswahl=0;
+						// auswahl=0;
 						break;
 					} else if (auswahl==1) {
 						state=MODUS_NETZGERAET;
-//						auswahl=0;
+						// auswahl=0;
 						break;
 					}
 				}
 				display_clear();
-				spi_write_string("Auswahl");
+				spi_write_pstr(PSTR("Auswahl"));
 				display_set_row(1);
 				if (auswahl==0) {
-					spi_write_string("Lader");
+					spi_write_pstr(PSTR("Lader"));
 				} else if (auswahl==1) {
-					spi_write_string("Netzteil");
+					spi_write_pstr(PSTR("Netzteil"));
 				}
 				delayms(100);
 				break;
@@ -371,32 +414,32 @@ void stateMachine(void) {
 				}
 				delayms(100);
 				display_clear();
-				spi_write_string("Modus: Lader");
+				spi_write_pstr(PSTR("Modus: Lader"));
 				display_set_row(1);
 				switch(state_lader) {
 					case LADESCHLUSSSPANNUNG:
-						spi_write_string("Ladeschlussspannung");
+						spi_write_pstr(PSTR("Ladeschlussspannung"));
 						if (knopf_losgelassen()) {
 							modus_lader_ladeschlussspannung = spannungseinstellung(modus_lader_ladeschlussspannung);
 						}
 						break;
 						
 					case MAXIMALSTROM:
-						spi_write_string("Maximalstrom");
+						spi_write_pstr(PSTR("Maximalstrom"));
 						if (knopf_losgelassen()) {
 							modus_lader_maximalstrom = stromeinstellung(modus_lader_maximalstrom);
 						}
 						break;
 						
 					case STROM_ENDE:
-						spi_write_string("Abschaltstrom");
+						spi_write_pstr(PSTR("Abschaltstrom"));
 						if (knopf_losgelassen()) {
 							modus_lader_strom_ende = stromeinstellung(modus_lader_strom_ende);
 						}
 						break;
 						
 					case START:
-						spi_write_string("Start");
+						spi_write_pstr(PSTR("Start"));
 						if (knopf_losgelassen()) {
 							state_lader=0;
 							state = LADEN_AKTIV;
@@ -404,7 +447,7 @@ void stateMachine(void) {
 						break;
 						
 					case ZURUECK:
-						spi_write_string("Zurück");
+						spi_write_pstr(PSTR("Zurück"));
 						if (knopf_losgelassen()) {
 							state_lader=0;
 							state = AUSWAHL;
@@ -415,10 +458,10 @@ void stateMachine(void) {
 				
 			case MODUS_NETZGERAET:
 				uartTxStrln("MODUS_NETZGERÄT");
-				static uint8_t modus_netzgeraet_auswahl = 0; //0=Spannung; 1=Maximalstrom; 2=start; 3=zurück
+				static uint8_t modus_netzgeraet_auswahl = 0; // 0=Spannung; 1=Maximalstrom; 2=start; 3=zurück
 				delayms(100);
 				display_clear();
-				spi_write_string("Modus: Netzgerät");
+				spi_write_pstr(PSTR("Modus: Netzgerät"));
 				display_set_row(1);
 				if (step_links() && (modus_netzgeraet_auswahl != 0)) {
 					modus_netzgeraet_auswahl--;
@@ -427,64 +470,44 @@ void stateMachine(void) {
 				}
 				switch (modus_netzgeraet_auswahl) {
 					case 0:
-						spi_write_string("Spannung");
+						spi_write_pstr(PSTR("Spannung"));
 						if (knopf_losgelassen()) {
 							netzgeraet_spannung = spannungseinstellung(netzgeraet_spannung);
 						}
 						break;
 						
 					case 1:
-						spi_write_string("Strom");
+						spi_write_pstr(PSTR("Strom"));
 						if (knopf_losgelassen()) {
-							netzgeraet_strom=stromeinstellung(netzgeraet_strom);
+							netzgeraet_strom = stromeinstellung(netzgeraet_strom);
 						}
 						break;
 						
 					case 2:
-						spi_write_string("Start");
+						spi_write_pstr(PSTR("Start"));
 						if (knopf_losgelassen()) {
 							state = REGELUNG_NETZGERAET;
 						}
 						break;
 						
 					case 3:
-						spi_write_string("zurück");
+						spi_write_pstr(PSTR("zurück"));
 						if (knopf_losgelassen()) {
 							modus_netzgeraet_auswahl=0;
 							state = AUSWAHL;
 						}
 						break;
 				}
-				
 				break;
 				
 			case REGELUNG_NETZGERAET:
-				NT_ON(1);
 				uartTxStrln("REGELUNG_NETZGERÄT");
-					display_set_row(1);
-					sprintf(display, "Uout: %5umV ", uNetzteil); // Netzteilspannung
-					spi_write_string(display);
-					display_set_row(2);
-					sprintf(display, "Iout: %5umA ", strom); // Laststrom
-					spi_write_string(display);
-					netzteil_regulation(netzgeraet_spannung, netzgeraet_strom);
-				if (knopf_losgelassen()) {
-					state=MODUS_NETZGERAET;
-					NT_ON(0);
-				}
-				delayms(100);				
+				netzteil_regulation();
+				// Funktion verlassen; Netzteil wird wieder ausgeschaltet.
+				state=MODUS_NETZGERAET;
+				NT_ON(0);
+				delayms(100);
 				break;
-				
-				//in Funktionen ausgelagert
-				/*      
-				 *   case SPANNUNGSEINSTELLUNG:
-				 *     break; 
-				 * 
-				 *   case STROMEINSTELLUNG:
-				 *     break; 
-				 * 
-				 *   case ZAHLENWERT_AENDERN:
-				 *     break; */
 				
 			case LADEN_AKTIV:
 				uartTxStrln("LADEN_AKTIV");
@@ -493,7 +516,7 @@ void stateMachine(void) {
 				if (knopf_losgelassen()) {
 					state = MODUS_LADER;
 				}
-				//TODO jump to ERROR_STATE; LADUNG_FERTIG
+				// TODO jump to ERROR_STATE; LADUNG_FERTIG
 				break;
 				
 			case LADUNG_FERTIG:
@@ -507,14 +530,15 @@ void stateMachine(void) {
 				
 			case ERROR_STATE:
 				uartTxStrln("ERROR_STATE");
+				NT_ON(0);
 				display_clear();
 				spi_write_string("Error");
+				delayms(100);
 				if (knopf_losgelassen()) {
 					state = AUSWAHL;
 				}
 				break;
-				
-		}	
+		}
 	}
 }
 
@@ -557,121 +581,15 @@ int main(void) {
 	LED(1);
 	delayms(1000);
 	
-	uint16_t sollspannung=30000, maximalstrom=10000; // Spannung in Millivolt, Strom in Milliampere
+// 	uint16_t sollspannung=30000, maximalstrom=10000; // Spannung in Millivolt, Strom in Milliampere
 	uint16_t tempspannung=0, tempstrom=0; // werte, die der Regler verändern kann.
 	// unabhängig von denen, die der User eingibt (maximalwerte)
-	
-	uint8_t status=0; // Statusvariabel für den Automaten
 	
 	sei(); // und es seien Interrupts (aktiv)!
 	
 	SPKR(1);
 	delayms(100);
 	SPKR(0);
-	
-	//setPowerOutput(24000);
-	
-	// 	while(1) {
-	// 		DOGM163_init(); // 3x16 Zeichen reflexives LCD
-	// 		display_clear();
-	// 		display_return_home();
-	// 		spi_write_string("Hodenkobold");
-	// 	}
-		
 	stateMachine();
 	return 0;
 }
-/*
-
-while(1) {
-	display_clear();
-	spi_write_string("  NUR ANZEIGE!  ");
-	
-	display_set_row(1);
-	sprintf(display, "Uout: %umV", uNetzteil); // Netzteilspannung
-	spi_write_string(display);
-	
-	display_set_row(2);
-	sprintf(display, "Iout: %umA", strom); // Laststrom
-	spi_write_string(display);
-	
-	delayms(100);
-}
-
-while(1) {
-	if (status == 0) { // init state: Netzteil Ausgang AUS
-		display_set_row(0);
-		spi_write_string("Netzteil INAKTIV");
-		uartTxStrln("NT aus");
-		NT_ON(0);
-		LED(0);
-	}
-	if (knopf & 1) { // wechsle in status
-		if (status == 0) {
-			status = 1;
-		} else if (status == 2) {
-			setPowerOutput(sollspannung);
-			NT_ON(1); // aktiviere netzteil
-			LED(1);
-			status = 3;
-		}
-	}
-	if (status == 1) { // spannung einstellen
-		display_set_row(0);
-		spi_write_string("U_soll stellen..");
-		uartTxStrln("U stellen");
-		status = 2;
-	}
-	if (status == 3) { // strom einstellen
-		display_set_row(0);
-		spi_write_string("I_max stellen..");
-		uartTxStrln("I stellen");
-		status = 4;
-	}
-	if ((knopf & 1<<2) || (knopf & 1<<3)) { // knopf gedreht um 1 nach rechts oder links
-		if (status == 2) { // spannung verstellen
-			display_set_row(1);
-			spi_write_string("Sollspannung:   ");
-			if (knopf & 1<<2) { // Knopf rechts gedreht
-				if(sollspannung<60000) {
-					sollspannung = sollspannung+100; // +100mV
-				}
-			} else if (knopf & 1<<3) { // Knopf links gedreht
-				if(sollspannung>12000) {
-					sollspannung = sollspannung-100; // -100mV
-				}
-			}
-			display_set_row(3);
-			sprintf(display, "Uout: %umV", sollspannung); // Sollspannung neu
-			spi_write_string(display);
-		}
-		if (status == 4) {
-			display_set_row(1);
-			spi_write_string("Maximalstrom:   ");
-			if (knopf & 1<<2) { // Knopf rechts gedreht
-				if(maximalstrom<55000) {
-					maximalstrom = maximalstrom+100; // +100mV
-				}
-			} else if (knopf & 1<<3) { // Knopf links gedreht
-				if(maximalstrom>0) {
-					maximalstrom = maximalstrom-100; // -100mV
-				}
-			}
-			display_set_row(3);
-			sprintf(display, "Umax: %umA", maximalstrom); // Sollspannung neu
-			spi_write_string(display);
-		}
-	}
-	if (status == 42) { // regler läuft.
-		// Regler läuft.
-		if (strom > maximalstrom) {
-			tempspannung--;
-		}
-		if (strom < maximalstrom) {
-			if (tempspannung < sollspannung) {
-				tempspannung++;
-			}
-		}
-		setPowerOutput(tempspannung);
-	}
-}*/
